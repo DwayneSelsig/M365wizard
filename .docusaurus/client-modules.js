1a249721371de0db6bd7f9a138ce92e0e7570b2c
Dwayne Selsig <dselsig@gmail.com>
2025-04-29 17:33:15 +0200

    .

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("R:\\Docu\\m365wizard\\node_modules\\infima\\dist\\css\\default\\default.css"),
+  require("R:\\Docu\\m365wizard\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
+  require("R:\\Docu\\m365wizard\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
+  require("R:\\Docu\\m365wizard\\src\\css\\custom.css"),
+];
