357b5bd86179360cb91c9a414b00624b1185573b
Dwayne Selsig <dselsig@hotmail.com>
2026-09-19 14:16:24 +0200

    Add postsPerPage configuration to blog settings

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -77,6 +77,7 @@ const config: Config = {
         blog: {
           showReadingTime: true,
           blogSidebarCount: 25,
+          postsPerPage: 5,
           feedOptions: {
             type: ['rss', 'atom'],
             xslt: true,
